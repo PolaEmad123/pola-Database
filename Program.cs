e04ca2b01f3099565be87a715026933ed873328e
polaemad##012 <emadpola93@gmail.com>
2023-01-25 23:53:42 +0200

    Functions con

--- Program.cs
+++ Program.cs
@@ -10,7 +10,8 @@ using System.Windows.Forms;
 
 namespace pola-Database
 {
-        public partial class Employee : Form
-        {
-            private Functions con;
-    public Employee()
+       public partial class Employee : Form
+       {
+    private Functions con;
+}
+}
